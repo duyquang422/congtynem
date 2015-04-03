e68c21560f6056a7fc4d221e01add5779cc96b5c
Tung-Sen <nobita28393@gmail.com>
2015-04-03 10:43:30 +0700

    thay 3 hinh nen slideshow

--- public/templates/public/system/html/includes/slideshow.php
+++ public/templates/public/system/html/includes/slideshow.php
@@ -1,9 +1,9 @@
 <div class="slide-container">
         <section id="dg-container" class="dg-container">
             <div class="dg-wrapper">
-                <a href="#"><img src="images/1.jpg" alt="image05"></a>
-                <a href="#"><img src="images/2.jpg" alt="image06"></a>
-                <a href="#"><img src="images/3.jpg" alt="image07"></a>
+                <a href="#"><img src="<?php echo $this->baseUrl('public/templates/public/system/images/4.jpg')?>" alt="image4"></a>
+                <a href="#"><img src="<?php echo $this->baseUrl('public/templates/public/system/images/5.JPG')?>" alt="image5"></a>
+                <a href="#"><img src="<?php echo $this->baseUrl('public/templates/public/system/images/6.jpg')?>" alt="image6"></a>
             </div>
             <nav>	
                 <span class="dg-prev">&lt;</span>
